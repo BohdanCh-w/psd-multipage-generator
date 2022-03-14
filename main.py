17670a3dcb5ddc7ee3105dd7c2bccbada89ada92
Bohdan Chepeliuk <BohdanCh.the.One@gmail.com>
2022-03-14 15:48:38 +0200

    Add precice compiler

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 """Startup point"""
 
+from http.server import executable
 import sys
+from os import listdir
 import shutil
 from config import Config
 from psd_service import PsdService
@@ -9,6 +11,8 @@ from psd_service import PsdService
 def validate(condition: bool, error: str) -> bool:
     """Validate condition"""
 
+    if callable(condition):
+        condition = condition()
     if not condition:
         print("Error: ", error)
         return False
@@ -19,11 +23,16 @@ def validate_config(config: Config) -> None:
     """Validate configuration check for existance"""
 
     requirements = [
-        (config.sourse.is_dir(), "specified sourse directory does not exist"),
-        (config.template.is_file(), "specified template file does not exist"),
-        (config.destination.parents[0].is_dir(), "specified destination folder does not exist"),
+        (config.sourse.exists(), "specified sourse directory does not exist"),
+        (config.sourse.is_dir(), "specified sourse is not a directory"),
+        (lambda: len(listdir(config.sourse)) > 0, "sourse directory is empty"),
+        #
+        (config.destination.parents[0].exists(), "specified destination folder does not exist"),
+        (config.destination.parents[0].is_dir(), "specified destination is not a directory"),
         (config.destination.name.endswith(".psd"), "destination is not a .psd file"),
-        (config.template.name.endswith(".psd"), "template is not a .psd file"),
+        #
+        (config.template.exists(), "specified template file does not exist"),
+        (config.template.name.endswith(".psd"), "specified template is not a .psd file"),
     ]
 
     for req in requirements:
@@ -35,10 +44,10 @@ def generate_file(config: Config) -> None:
     """Use Psd Service to generate output file"""
 
     file = str(config.destination.resolve())
-    srv = PsdService()
+    srv = PsdService(config.template, config.destination)
 
     shutil.copyfile(config.template, config.destination)
-    srv.populate(file, config.sourse, False)
+    srv.populate(file, config.sourse, True)
 
 
 def main():
--- psd_service.py
+++ psd_service.py
@@ -1,6 +1,7 @@
 """Adobe Photoshop Service"""
 
 from os import listdir
+import shutil
 import win32com.client
 from photoshop import Session
 
@@ -10,8 +11,10 @@ class PsdService:
 
     SILENT_CLOSE = 2
 
-    def __init__(self):
+    def __init__(self, sample="", destination=""):
         self.app = win32com.client.Dispatch("Photoshop.Application")
+        self.sample = sample
+        self.destination = destination
 
     def populate(self, file: str, sourse: str, flexible: bool) -> None:
         """Generate psd"""
@@ -21,16 +24,45 @@ class PsdService:
         else:
             self.__populate_precise(file, sourse)
 
-    def __copy_file_contents_to_clipboard(self, path):
+    def __copy_file_contents_to_clipboard(self, path, size=None):
         doc = self.app.Open(path)
         doc.Layers[0].Copy()
+
+        ret = True
+        if size is not None:
+            actual_size = (doc.width, doc.height)
+            ret = (size == actual_size, actual_size)
+
         doc.Close(self.SILENT_CLOSE)
+        return ret
 
     def __create_layer_from_file(self, doc, layer, path):
         self.__copy_file_contents_to_clipboard(path)
 
         doc.ActiveLayer = layer
         doc.Paste()
+
+    def __create_layer_or_new_doc_from_file(self, doc, layer, path, size):
+        ok, size = self.__copy_file_contents_to_clipboard(path, size)
+        if ok:
+            doc.ActiveLayer = layer
+            doc.Paste()
+            return True
+        else:
+            img_name = "".join(path.name.split(".")[:-1])
+            new_file = self.destination.parents[0] / (img_name + ".psd")
+
+            shutil.copyfile(self.sample, new_file)
+            doc = self.app.Open(new_file.resolve())
+
+            doc.ResizeCanvas(size[0], size[1])
+            doc.Layers[0].Name = img_name
+            doc.ActiveLayer = doc.Layers[0].Layers[2]
+            doc.Paste()
+
+            doc.Save()
+            doc.Close(self.SILENT_CLOSE)
+            return False
 
     def __populate_easy_mode(self, file: str, sourse: str):
         self.app.Open(file)
@@ -59,7 +91,36 @@ class PsdService:
         doc.Save()
         doc.Close(self.SILENT_CLOSE)
 
-    def __generate_layers(self, file: str, sourse: str):
+    def __populate_precise(self, file: str, sourse: str):
+        self.app.Open(file)
+        doc = self.app.Application.ActiveDocument
+        images = listdir(sourse)
+        doc.ActiveLayer = doc.Layers[0]
+        size = (doc.width, doc.height)
+
+        for _ in range(1, len(images)):
+            doc.ActiveLayer.Duplicate()
+
+        for i, img in enumerate(images):
+            layer = doc.Layers[i]
+            imgname = "".join(img.split(".")[:-1])
+            layer.Name = imgname
+
+            layer.Layers[0].Name = "text"
+            layer.Layers[1].Name = "clean"
+            layer.Layers[2].Name = imgname
+
+            layer.Visible = False
+            if not self.__create_layer_or_new_doc_from_file(doc, layer.Layers[2], (sourse / img).resolve(), size):
+                layer.Delete()
+
+        doc.Layers[0].Visible = True
+        self.app.ExecuteAction(self.app.StringIDToTypeID("collapseAllGroupsEvent"))
+
+        doc.Save()
+        doc.Close(self.SILENT_CLOSE)
+
+    def __generate_layers(self, file: str, sourse: str):  # pylint: disable=unused-private-member
         images = listdir(sourse)
 
         with Session(file, action="open") as psd:
@@ -74,6 +135,3 @@ class PsdService:
 
             doc.saveAs(file, psd.PhotoshopSaveOptions(), True)
             doc.close()
-
-    def __populate_precise(self, file: str, sourse: str):
-        raise NotImplementedError
