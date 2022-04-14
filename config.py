791bcbc25ce2bc742b2493372060e75bcba28142
Bohdan Chepeliuk <BohdanCh.the.One@gmail.com>
2022-04-15 01:43:12 +0300

    Update config

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
-"""Configuration for application"""
+'''Configuration for application'''
 from pathlib import Path
 
 import yaml
 
 
 class Config:
-    """App configuration"""
+    '''App configuration'''
 
-    __srs = "source"
-    __dst = "destination"
-    __tpl = "template"
-    __prc = "precise"
+    __srs = 'source'
+    __dst = 'destination'
+    __tpl = 'template'
+    __prc = 'precise'
 
     def __init__(self, path: str = None):
         if path is None:
@@ -22,37 +22,50 @@ class Config:
             self.read_from_file(path)
 
     def read_from_file(self, path: str) -> None:
-        """Read configuration from file specified by path"""
+        '''Read configuration from file specified by path'''
 
         try:
-            with open(path, "r", encoding="utf-8") as f:
-                data = yaml.safe_load(f)["config"]
-
-                for key in (self.__srs, self.__tpl, self.__prc):
-                    if key not in data:
-                        raise KeyError(f"{key} pararmeter is missing")
-                    if "value" not in data[key]:
-                        raise KeyError(f"value missing in {key}")
-
-                self.source = Path(data[self.__srs]["value"])
-                self.template = Path(data[self.__tpl]["value"])
-                self.precise = data[self.__prc]["value"]
-
-                if data.get(self.__dst) is None:
-                    self.destination = self.source.parents[0] / "output.psd"
-                    return
-
-                if data[self.__dst]["value"] != "":
-                    self.destination = Path(data[self.__dst]["value"])
+            with open(path, 'r', encoding='utf-8') as f:
+                data: dict = yaml.safe_load(f)
+                if 'config' in data.keys():
+                    self._parse_config_v1(data)
                 else:
-                    self.destination = self.source.parents[0] / "output.psd"
+                    self._parse_config_v2(data)
 
         except FileNotFoundError:
-            print("Configuration file not found")
+            print('Configuration file not found')
             exit(1)
         except KeyError as err:
-            print("Bad configuration file - key is missing : " + str(err))
+            print('Bad configuration file - key is missing : ' + str(err))
             exit(1)
 
-        if self.destination is None:
-            self.destination = self.source.parents[0]
+    def _parse_config_v1(self, data: dict) -> None:
+        data = data['config']
+        for key in (self.__srs, self.__tpl, self.__prc):
+            if key not in data:
+                raise KeyError(f'{key} pararmeter is missing')
+            if 'value' not in data[key]:
+                raise KeyError(f'value missing in {key}')
+
+        self.source = Path(data[self.__srs]['value'])
+        self.template = Path(data[self.__tpl]['value'])
+        self.precise = data[self.__prc]['value']
+
+        if not data.get(self.__dst):
+            self.destination = self.source.parents[0] / 'output.psd'
+        else:
+            self.destination = Path(data[self.__dst])
+
+    def _parse_config_v2(self, data: dict) -> None:
+        for key in (self.__srs, self.__tpl, self.__prc):
+            if key not in data:
+                raise KeyError(f'{key} pararmeter is missing')
+
+        self.source = Path(data[self.__srs])
+        self.template = Path(data[self.__tpl])
+        self.precise = data[self.__prc]
+
+        if not data.get(self.__dst):
+            self.destination = self.source.parents[0] / 'output.psd'
+        else:
+            self.destination = Path(data[self.__dst])
